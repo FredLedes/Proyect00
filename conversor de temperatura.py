c171d509ec6d3bf71f67d240f5affde456af88b6
Federico Ledesma <160787781+FredLedes@users.noreply.github.com>
2024-03-06 17:35:13 +0000

    Update conversor de temperatura.py

--- conversor de temperatura.py
+++ conversor de temperatura.py
@@ -3,26 +3,19 @@
 #Grados centígrados = (grados Fahrenheit − 32) × 5/9.
 #Grados Fahrenheit = (grados centígrados × 9/5) +32
 
-#Formula para calcular Centigrados
-def calcel(t):
-    return int((t-32)*(5/9))
+#ingreso de datos. Qué variables necesito tener?
+choise = input('Elige sólo con la letra \n\nQuieres obtener Fahrenheit(F) o Celsius(C)?: ').lower()
 
-#Formula para calcular Fahrenheit
-def calfahr(t):
-    return int(t*(9/5)+32)
+temp = float(input('Ingresa los grados de temperatura: '))
 
-choise = input('Elige sólo con la letra \n\nQué unidad quieres obtener? Fahrenheit(F) o Celsius(C)?: ')
-choise = choise.lower()
-
-temp = input('Ingresa los grados de temperatura: ')
-temp = int(temp)
-
+#Comportamiento - Qué voy a hacer con los datos ingresados?
 if choise == 'c':
     print('A- La temperatura es: ')
-    print(calcel(temp),'grados centígrados')
+    print((temp-32)*5/9,'grados centígrados')
 elif choise == 'f':
     print('B- La temperatura es: ')
-    print(calfahr(temp),'grados farherenheit')
+    print(temp*(9/5)+32,'grados farherenheit')
 else:
     print('Discula, debes elegir unicamente entre Fahrenheit(F) o Celsius(C)')
+
 
